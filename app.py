bcefe61502b3e742a4ae595af3ecceebc74cb2a2
Terresapan <terresap2010@gmail.com>
2024-09-26 05:59:39 +0000

    minor ui change

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from graph_logic import setup_critique_graph
+from graph_logic import setup_critique_graph, reflectionstate
 from langchain_groq import ChatGroq
 from utils import save_feedback
 
@@ -35,7 +35,7 @@ if st.sidebar.button("Submit Feedback"):
 st.sidebar.image("assets/icon01.png", use_column_width=True)
 
 # ask user for their OpenAI API key via `st.text_input`.
-groq_api_key = st.text_input("Groq API Key", type="password")
+groq_api_key = st.text_input("Groq API Key", type="password", placeholder="Your Groq API Key here...")
 if not groq_api_key:
     st.info("Please add your Groq API key to continue.", icon="🗝️")
 else:
@@ -44,9 +44,9 @@ else:
 
     # input field for user to input a product description
     st.header("Enter Product Details")
-    product_description_input = st.text_input("Enter a Product/Service Description (e.g., organic soup)")
-    target_audience_input = st.text_input("Enter the Target Audience (e.g., for working adults):")
-    selling_point_input = st.text_input("Enter Key Selling Points (e.g., ready in 5 minutes):")
+    product_description_input = st.text_input("Enter a Product/Service Description", placeholder="organic soup")
+    target_audience_input = st.text_input("Enter the Target Audience", placeholder="for busy working adults")
+    selling_point_input = st.text_input("Enter Key Selling Points", placeholder="ready in 5 minutes")
 
     # button to trigger tagline generation
     if st.button("Generate Tagline"):
@@ -55,10 +55,7 @@ else:
             st.error("Please fill out all fields to generate a tagline.", icon="🚫")
         else:
             # Set up the initial state (reflectionstate object)
-            input_state = {"product_description": product_description_input,
-                           "target_audience": target_audience_input,
-                           "selling_point": selling_point_input,
-                           "messages": []}
+            input_state = reflectionstate
             # Setup graph and inject model
             critique_graph = setup_critique_graph(model)
             # Invoke the compiled debate graph
